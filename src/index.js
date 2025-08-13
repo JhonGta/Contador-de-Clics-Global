3bddb7e082b3c9752832a8eccf7d4b3e6e07a7ee
JhonGta <jhonguaman-10@hotmail.com>
2025-08-13 08:36:36 -0500

    pr

--- src/index.js
+++ src/index.js
@@ -76,3 +76,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log('Servidor escuchando en puerto', PORT);
 });
+
+// Exportar app para Vercel serverless
+export default app;
